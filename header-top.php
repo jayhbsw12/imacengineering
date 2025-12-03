7df052eb2f0a543242e628557d77cb54e6c9b032
jay.m@hbsoftweb.in <jay.m@hbsoftweb.in>
2025-12-03 13:47:42 +0530

    updated header-top.php

--- header-top.php
+++ header-top.php
@@ -25,7 +25,6 @@ if (file_exists($sourceFile)) {
 <html lang="en">
 
 <head>
-    <base href="https://imacengineering.com/">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="x-dns-prefetch-control" content="on">

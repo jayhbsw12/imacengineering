f8c115e6a28cee4536f8b72d30d2422656ddf305
info@hbsoftweb.com <info@hbsoftweb.com>
2026-02-20 11:07:40 +0530

    updated google tag manager

--- header.php
+++ header.php
@@ -474,6 +474,16 @@ if (!empty($JSON_LD_SCRIPTS) && is_array($JSON_LD_SCRIPTS)) {
 // =====================================================================
 ?>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F2Y7548TW5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-F2Y7548TW5');
+</script>
+ 
+
 </head>
 
 <body>
